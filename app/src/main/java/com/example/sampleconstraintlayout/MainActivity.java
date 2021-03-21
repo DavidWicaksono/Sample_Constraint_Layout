cb912e4826a2e157b335552962e6919e9c8478bf
David Wicaksono <david.wicaksono.ft18@mail.umy.ac.id>
2021-03-22 03:10:59 +0700

    eintent dan passing parameter done.

--- app/src/main/java/com/example/sampleconstraintlayout/MainActivity.java
+++ app/src/main/java/com/example/sampleconstraintlayout/MainActivity.java
@@ -49,27 +49,62 @@ public class MainActivity extends AppCompatActivity {
                 //menyimpan input user di edittext password kedalam variabel password
                 password = edpassword.getText().toString();
 
+                String email = "admin@mail.com";
+
+                //mengeset password yang benar
+                String pass = "123";
+
                 //validasi input
                 if (TextUtils.isEmpty(edemail.getText().toString()) || TextUtils.isEmpty(edpassword.getText().toString())) {
                     Toast.makeText(MainActivity.this, "Email dan Password Salah", Toast.LENGTH_LONG).show();
-                }else if(edemail.getText().toString().equals(correct_edemail)){
+                } else if (edemail.getText().toString().equals(correct_edemail)) {
                     //check password
-                    if(edpassword.getText().toString().equals(correct_edpassword)){
-                        Toast.makeText(MainActivity.this,"Login Sukses",Toast.LENGTH_LONG).show();
+                    if (edpassword.getText().toString().equals(correct_edpassword)) {
+                        Toast.makeText(MainActivity.this, "Login Sukses", Toast.LENGTH_LONG).show();
 
-                    }else{
-                        Toast.makeText(MainActivity.this,"Password Salah",Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(MainActivity.this, "Password Salah", Toast.LENGTH_LONG).show();
                     }
-                }else{
-                    Toast.makeText(MainActivity.this,"Email Salah",Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Email Salah", Toast.LENGTH_LONG).show();
                 }
-                //membuat variabel toast dan membuat toast dengan menambahkan variabel nama dan password
-                Toast t = Toast.makeText(getApplicationContext(),
-                        "email anda: " + nama + " dan Password anda: " + password + "", Toast.LENGTH_LONG);
-                //menampilkan toast
-                t.show();
-            }
 
+                if (nama.equals(email) && password.equals(pass)) {
+                    //membuat variabel toast dan membuat toast dengan menambahkan variabel nama dan password
+                    Toast t = Toast.makeText(getApplicationContext(),
+                            "email anda: " + nama + " dan Password anda: " + password + "", Toast.LENGTH_LONG);
+                    //menampilkan toast
+                    t.show();
+
+                    //Membuat objek bundle
+                    Bundle b = new Bundle();
+
+                    //memasukkan value dari variabel password dengan kunci "b"
+                    //dan masukkan kedalam bundle
+                    b.putString("a", nama.trim());
+
+                    //memasukkan value dari variabel password dengan kunci "b"\
+                    //dan dimasukkan kedalam bundle
+                    b.putString("b", password.trim());
+
+                    //membuat objek intent berpindah actiity dari mainactivity ke ActivityHasil
+                    Intent i = new Intent(getApplicationContext(), ActivityHasil.class);
+
+                    //memasukkan bundle kedalam intent untuk dikirimkan ke ActivityHasi
+                    i.putExtras(b);
+
+                    //berpindah ke ActivityHasil
+                    startActivity(i);
+                } else {
+                    //membuat variabel toast dan membuat toast dan menampilkan pesan "Login gagal
+
+                    Toast t = Toast.makeText(getApplicationContext(),
+                            "Login Gagal", Toast.LENGTH_LONG);
+
+                    //menampilkan toast
+                    t.show();
+                }
+            }
         });
     }
 }
